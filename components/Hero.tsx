4f2ffb629092509664ee11b7ef6a2c3c6798619f
Santiago Zappa <santiagozappab@gmail.com>
2024-05-22 17:19:25 -0500

    first

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='flex flex-col items-center justify-center space-y-3'>
+        <h1>Hello welcome to slangup, here you will discover a new way to learn english</h1>
+        <button className='bg-slate-700 text-white rounded-md px-5'>Telegram</button>
+        <button className='bg-slate-700 text-white rounded-md px-5'>Whatsapp</button>
+       
+    </div>
+  )
+}
+
+export default Hero
