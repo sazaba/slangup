a419a9a9adfe8491137fd33489dd52cc8619018f
Santiago Zappa <santiagozappab@gmail.com>
2024-05-22 18:02:30 -0500

    meta2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import Hero from "@/components/Hero";
-import Metapixel from "@/components/Metapixel"
-import Image from "next/image";
 
 export default function Home() {
   return (
   <>
   <Hero/>
-  <Metapixel/>
   </>
   );
 }
